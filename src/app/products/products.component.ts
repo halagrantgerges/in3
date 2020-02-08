418eea7204353aa4c4ce752e743bcd27bdd958a1
hala <halagrant@hotmail.com>
2020-02-08 20:46:04 +0200

    adding paginator

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,37 +1,69 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductsService } from './products.service';
-import { Product } from './product';
-import {MatTableDataSource} from '@angular/material/table';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { ProductsService } from "./products.service";
+import { Product } from "./product";
+import { MatTableDataSource } from "@angular/material/table";
+import { MatSort, MatPaginator } from "@angular/material";
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  selector: "app-products",
+  templateUrl: "./products.component.html",
+  styleUrls: ["./products.component.css"]
 })
 export class ProductsComponent implements OnInit {
+  displayedColumns: string[] = ["id", "Company", "Product"];
+  data: Product[] = [];
+  isLoadingResults = true;
+  dataSource = new MatTableDataSource(this.data);
+  @ViewChild(MatSort) sort: MatSort;
+  constructor(private productsApi: ProductsService) {}
 
-  displayedColumns: string[] = ['id','Company', 'Product'];
-data: Product[] = [];
-isLoadingResults = true;
-dataSource = new MatTableDataSource(this.data);
+  @ViewChild(MatSort) set matSort(ms: MatSort) {
+    this.sort = ms;
+    this.setDataSourceAttributes();
+     }
+     @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor(private productsApi: ProductsService) { }
+    
+    // @ViewChild(MatPaginator) set matPaginator(mp: MatPaginator) {
+    //  this.matPaginator = mp;
+    // this.setDataSourceAttributes();
+    //  }
+    
+    setDataSourceAttributes() {
+      // this.dataSource.paginator = this.matPaginator;
+      this.dataSource.sort = this.sort;
+      }
 
+ 
   ngOnInit() {
-
-  this.productsApi.getProducts()
-  .subscribe(res => {
-    this.data = res;
-    console.log(this.data);
-    this.isLoadingResults = false;
-  }, err => {
-    console.log(err);
-    this.isLoadingResults = false;
-  });
+    this.data=[{id:0, Company: '',
+      Product: '',
+      Type: '',
+      Inches: '',
+      Resolution: '',
+      CPU: '',
+      RAM: '',
+      Memory: '',
+      Graphics: '',
+      OpSys:'',
+      Weight: '',
+      Price: ''}];
+     
+    this.productsApi.getProducts().subscribe(
+      res => {
+        this.data = res;
+        this.dataSource.paginator = this.paginator;
+        console.log(this.data);
+        this.isLoadingResults = false;
+      },
+      err => {
+        console.log(err);
+        this.isLoadingResults = false;
+      }
+    );
   }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
-
 }
